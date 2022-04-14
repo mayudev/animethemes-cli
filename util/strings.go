41741ae9836b91cbf3709a821dee09c1e08fdbe8
mayudev <99561108+mayudev@users.noreply.github.com>
2022-04-14 18:38:07 +0200

    feat: display video tags when there's only one entry

--- util/strings.go
+++ util/strings.go
@@ -7,21 +7,35 @@ import (
 	"github.com/pterm/pterm"
 )
 
+func BuildThemeString(t *api.AnimeTheme) string {
+	result := t.Slug + " " + pterm.Sprintf(pterm.LightYellow(t.Song.Title))
+
+	if len(t.Entries) == 1 && len(t.Entries[0].Videos) == 1 {
+		result += " [" + BuildVideoChoiceString(&t.Entries[0].Videos[0]) + "]"
+	}
+
+	return result
+}
+
 // BuildEntryChoiceString builds a string to be shown to user in selection
 // displaying all information about an entry
 func BuildEntryChoiceString(e *api.AnimeThemeEntry) string {
 	result := ""
 
 	if len(e.Episodes) > 0 {
-		result += "[" + pterm.LightBlue(e.Episodes) + "] "
+		result += "[" + pterm.LightBlue(e.Episodes) + "]"
 	}
 
 	if e.Nsfw {
-		result += pterm.Red("NSFW") + " "
+		result += " " + pterm.Red("NSFW")
 	}
 
 	if e.Spoiler {
-		result += pterm.Red("Spoiler") + " "
+		result += " " + pterm.Red("Spoiler")
+	}
+
+	if len(e.Videos) == 1 {
+		result += " [" + BuildVideoChoiceString(&e.Videos[0]) + "]"
 	}
 
 	return result
@@ -33,23 +47,23 @@ func BuildVideoChoiceString(v *api.Video) string {
 	result := ""
 
 	if v.Resolution != 0 {
-		result += pterm.LightBlue(strconv.Itoa(v.Resolution)+"p") + " "
+		result += pterm.LightBlue(strconv.Itoa(v.Resolution) + "p")
 	}
 
 	if len(v.Source) > 0 {
-		result += pterm.LightMagenta(v.Source) + " "
+		result += " " + pterm.LightMagenta(v.Source)
 	}
 
 	if v.NC {
-		result += pterm.LightGreen("Creditless") + " "
+		result += " " + pterm.LightGreen("Creditless")
 	}
 
 	if v.Lyrics {
-		result += pterm.LightYellow("Lyrics") + " "
+		result += " " + pterm.LightYellow("Lyrics")
 	}
 
 	if v.Overlap != "None" {
-		result += "Transition: " + pterm.LightRed(v.Overlap)
+		result += " " + pterm.LightRed(v.Overlap)
 	}
 
 	return result
